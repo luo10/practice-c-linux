a5e668690642f6867c4ab1f0d61e4f5967e39797
罗十十十 <330240995@qq.com>
2022-11-06 15:29:17 +0800

    feat: open-close

---
+++ A02open_close.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+// ./a.out <pathname>
+int main(int argc, char *argv[])
+{
+
+  int fd;
+  // 判断是否有两个参数
+  if (argc != 2)
+  {
+    fprintf(stderr, "Usage : <%s> <pathname>\n", argv[0]);
+    return -1;
+  }
+
+  // fd = open(argv[1], O_RDONLY);
+  fd = open(argv[1], O_WRONLY | O_CREAT | O_TRUNC, 0644); // 只写 | 创建 | 截短
+
+  // 如果 = -1 则报错了
+  if (fd == -1)
+  {
+    perror("open(): ");
+    return -1;
+  }
+  close(fd);
+
+  printf(" fd = %d\n", fd);
+
+  return 0;
+}
